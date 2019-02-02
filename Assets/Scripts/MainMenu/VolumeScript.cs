b18106e8405ec001dbbd095be2899ceae7a58aba
PityYouWeak <homie.pajimula@yahoo.com>
2019-02-02 14:49:49 +0800

    remove warning

--- Assets/Scripts/MainMenu/VolumeScript.cs
+++ Assets/Scripts/MainMenu/VolumeScript.cs
@@ -6,7 +6,6 @@ public class VolumeScript : MonoBehaviour, IEndDragHandler {
 
     public AudioSource music;
     public Slider volume;
-    float volumeValue = 1;
 
     public void OnEndDrag(PointerEventData eventData)
     {
